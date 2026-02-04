343a0906da3fb6a5ca5dcd2a8fc46321f1f70e68
Joao <joao.v.arbegaus@gmail.com>
2026-02-04 14:07:04 -0300

    criação de alguns domains

---
+++ src/main/java/dev/wakanda/produdoro/usuario/domain/Usuario.java
@@ -0,0 +1,30 @@
+package dev.wakanda.produdoro.usuario.domain;
+
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(collection = "Usuario")
+@Getter
+public class Usuario {
+
+    @Id
+    private UUID idUsuario;
+    @Email
+    @Indexed
+    private String email;
+    private ConfiguracaoUsuario configuracao;
+    @Builder.Default
+    private StatusUsuario status = StatusUsuario.FOCO;
+    @Builder.Default
+    private Integer quantidadePomodoroPausaCurta = 0;
+
+
+}

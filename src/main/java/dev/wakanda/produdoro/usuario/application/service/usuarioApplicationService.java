40afc5cf8aa9b84ebc4bc2056ea5d64f20e280ee
Joao <joao.v.arbegaus@gmail.com>
2026-02-06 02:17:32 -0300

    teste 2 assim funciona o swagger

--- src/main/java/dev/wakanda/produdoro/usuario/application/service/usuarioApplicationService.java
+++ src/main/java/dev/wakanda/produdoro/usuario/application/service/usuarioApplicationService.java
@@ -1,8 +1,8 @@
-package dev.wakanda.produdoro.usuario.application.service;
-
-import dev.wakanda.produdoro.usuario.application.api.UsuarioCriadoResponse;
-import dev.wakanda.produdoro.usuario.application.api.UsuarioNovoRequest;
-
-public interface usuarioApplicationService {
-    UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
-}
+//package dev.wakanda.produdoro.usuario.application.service;
+//
+//import dev.wakanda.produdoro.usuario.application.api.UsuarioCriadoResponse;
+//import dev.wakanda.produdoro.usuario.application.api.UsuarioNovoRequest;
+//
+//public interface usuarioApplicationService {
+//    UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
+//}
